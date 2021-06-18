6cf47fac883a8be1a1c1c02e1b34ec537f761f04
Kohki Takata <koki@Kokis-iMac.local>
2021-06-19 01:58:47 +0900

    test upload

--- upload.php
+++ upload.php
@@ -11,13 +11,13 @@ include("includes/header.php");
 			'region'  => '-- your region --',
 			'version' => 'latest',
 			'credentials' => [
-				'key'    => "-- access key id --",
-				'secret' => "-- secret access key --",
+				'key'    => "getenv('AWS_ACCESS_KEY_ID')",
+				'secret' => "getenv('AWS_SECRET_ACCESS_KEY')",
 			]
 		]);		
 
 		$result = $s3->putObject([
-			'Bucket' => '-- bucket name --',
+			'Bucket' => 'getenv("S3_BUCKET")',
 			'Key'    => $file_name,
 			'SourceFile' => $temp_file_location			
 		]);
@@ -26,9 +26,19 @@ include("includes/header.php");
 	}
 ?>
 
-<form action="upload.php" method="post"  enctype="multipart/form-data">
-	Upload something<br /><br />
-	<input type="file" id="image" name="image" style="width:200px; height:30px; " /><br /><br />
-	<input type="submit" value="Submit" style="width:85px; height:25px;" />
-</form><br /><br />
-<br /><br />
+<div id="Overlay" style=" width:100%; height:100%; border:0px #990000 solid; position:absolute; top:0px; left:0px; z-index:2000; display:none;"></div>
+<div class="main_column column">
+
+
+	<div id="formExample">
+		
+	    <p><b> <?=$msg?> </b></p>
+	    
+	    <form action="upload.php" method="post"  enctype="multipart/form-data">
+	        Upload something<br /><br />
+	        <input type="file" id="image" name="image" style="width:200px; height:30px; " /><br /><br />
+	        <input type="submit" value="Submit" style="width:85px; height:25px;" />
+	    </form><br /><br />
+	    
+	</div> <!-- Form-->  
+</div>
