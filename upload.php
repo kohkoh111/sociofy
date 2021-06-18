46de8678d54b41bb445c9e727125cb9b039b331c
Kohki Takata <koki@Kokis-iMac.local>
2021-06-19 01:43:05 +0900

    test upload

--- upload.php
+++ upload.php
@@ -1,78 +1,34 @@
 <?php 
 include("includes/header.php");
-// require_once "vendor/autoload.php";
 
-// use Aws\S3\S3Client;
+	if(isset($_FILES['image'])){
+		$file_name = $_FILES['image']['name'];   
+		$temp_file_location = $_FILES['image']['tmp_name']; 
 
-// $profile_id = $user['username'];
-// $imgSrc = "";
-// $result_path = "";
-// $msg = "";
+		require 'vendor/autoload.php';
 
-// //S3使用準備
-// $bucket = '--- BUCKET_NAME ---';
-// $key = '--- KEY_NAME ---';
-// $secret = '--- SECRET_NAME ---';
+		$s3 = new Aws\S3\S3Client([
+			'region'  => '-- your region --',
+			'version' => 'latest',
+			'credentials' => [
+				'key'    => "-- access key id --",
+				'secret' => "-- secret access key --",
+			]
+		]);		
 
+		$result = $s3->putObject([
+			'Bucket' => '-- bucket name --',
+			'Key'    => $file_name,
+			'SourceFile' => $temp_file_location			
+		]);
 
-// //S3インスタンス作成
-// s3 = new S3Client(array(
-//     'version' => 'latest',
-//     'credentials' => array(
-//         'key' => $key,
-//         'secret' => $secret,
-//     ),
-//     'region'  => 'ap-northeast-3', // 大阪地域設定
-// ));
-
-// /***********************************************************
-// 	0 - tmpの画像があれば削除
-// ***********************************************************/
-// 	if (!isset($_POST['x']) && !isset($_FILES['image']['name']) ){
-// 		//Delete users temp image
-// 			$temppath = 'assets/images/profile_pics/'.$profile_id.'_temp.jpeg';
-// 			if (file_exists ($temppath)){ @unlink($temppath); }
-// 	} 
-
-
-// if(isset($_FILES['image']['name'])){	
-// /***********************************************************
-// 	1 - S3(AWS)へアップロード
-// ***********************************************************/	
-// // アップロードされた画像の処理
-// $file = $_FILES['image']['tmp_name'];
-// if (!is_uploaded_file($file)) {
-//     return;
-// }
-
-// // S3バケットに画像をアップロード
-// $result = $s3->putObject(array(
-//     'Bucket' => $bucket,
-//     'Key' => time() . '.jpg',
-//     'Body' => fopen($file, 'rb'),
-//     'ACL' => 'public-read', // 画像は一般公開されます
-//     'ContentType' => mime_content_type($file),
-// ));
-// }
+		var_dump($result);
+	}
 ?>
 
-
-<div id="Overlay" style=" width:100%; height:100%; border:0px #990000 solid; position:absolute; top:0px; left:0px; z-index:2000; display:none;"></div>
-<div class="main_column column">
-
-
-	<div id="formExample">
-		
-	    <p><b> <?=$msg?> </b></p>
-	    
-	    <form action="upload.php" method="post"  enctype="multipart/form-data">
-	        Upload something<br /><br />
-	        <input type="file" id="image" name="image" style="width:200px; height:30px; " /><br /><br />
-	        <input type="submit" value="Submit" style="width:85px; height:25px;" />
-	    </form><br /><br />
-	    
-	</div> <!-- Form--> 
-      
- 	 
- 
-    <br /><br />
+<form action="upload.php" method="post"  enctype="multipart/form-data">
+	Upload something<br /><br />
+	<input type="file" id="image" name="image" style="width:200px; height:30px; " /><br /><br />
+	<input type="submit" value="Submit" style="width:85px; height:25px;" />
+</form><br /><br />
+<br /><br />
