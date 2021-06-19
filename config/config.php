1fc4524dacab243203f4f058867d238a7d4b2d1c
Kohki Takata <koki@Kokis-iMac.local>
2021-06-19 12:45:49 +0900

    js script in index.php revised

--- config/config.php
+++ config/config.php
@@ -6,23 +6,16 @@ $timezone = date_default_timezone_set("Asia/Tokyo");
 
 
 // ローカル開発環境
-//$con = mysqli_connect("localhost", "root", "root", "social",8889); //Connection variable
+//$con = mysqli_connect("localhost", "root", "root", "social",8889); 
 
 
 // 本番環境用
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $server = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $db = substr($url["path"], 1);
-
-// $con = mysqli_connect($server, $username, $password, $db);
-
 $servername = "localhost";
 $username = "qcxmfjwfwv";
 $password = "CxDu9p4bUT";
 $db = "qcxmfjwfwv";
-// Create connection
+
+//接続用
 $con = mysqli_connect($servername, $username, $password,$db);
 
 if(mysqli_connect_errno())
--- index.php
+++ index.php
@@ -43,58 +43,68 @@ if(isset($_POST['post'])){
 
 	</div>
 
-	<script>
+<script>
+$(function(){
+
 	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+	var inProgress = false;
 
-	$(document).ready(function() {
+	loadPosts(); //Load first posts
 
+    $(window).scroll(function() {
+    	var bottomElement = $(".status_post").last();
+    	var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+        // isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
+        if (isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+            loadPosts();
+        }
+    });
+
+    function loadPosts() {
+        if(inProgress) { //If it is already in the process of loading some posts, just return
+			return;
+		}
+		
+		inProgress = true;
 		$('#loading').show();
 
-		//Original ajax request for loading first posts
+		var page = $('.posts_area').find('.nextPage').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
+
 		$.ajax({
 			url: "includes/handlers/ajax_load_posts.php",
 			type: "POST",
-			data: "page=1&userLoggedIn=" + userLoggedIn,
+			data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
 			cache:false,
 
-			success: function(data) {
+			success: function(response) {
+				$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
+				$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
+				$('.posts_area').find('.noMorePostsText').remove(); //Removes current .nextpage 
+
 				$('#loading').hide();
-				$('.posts_area').html(data);
+				$(".posts_area").append(response);
+
+				inProgress = false;
 			}
 		});
+    }
 
-		$(window).scroll(function() {
-			var height = $('.posts_area').height(); //Div containing posts
-			var scroll_top = $(this).scrollTop();
-			var page = $('.posts_area').find('.nextPage').val();
-			var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+    //Check if the element is in view
+    function isElementInView (el) {
+        var rect = el.getBoundingClientRect();
 
-			if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
-				$('#loading').show();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
+        );
+    }
+});
 
-				var ajaxReq = $.ajax({
-					url: "includes/handlers/ajax_load_posts.php",
-					type: "POST",
-					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-					cache:false,
+</script>
 
-					success: function(response) {
-						$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage
-						$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage
-
-						$('#loading').hide();
-						$('.posts_area').append(response);
-					}
-				});
-
-				} //End if
-
-			return false;
-
-			}); //End (window).scroll(function())
-		});
-
-	</script>
 
 
 
