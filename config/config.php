7fd8c1ec273d562526ef3e21d2659d8ec4aed037
Kohki Takata <koki@Kokis-iMac.local>
2021-06-23 19:52:06 +0900

    style revised

--- config/config.php
+++ config/config.php
@@ -6,17 +6,17 @@ $timezone = date_default_timezone_set("Asia/Tokyo");
 
 
 // ローカル開発環境
-//$con = mysqli_connect("localhost", "root", "root", "social",8889); 
+$con = mysqli_connect("localhost", "root", "root", "social",8889); 
 
 
 // 本番環境用
-$servername = "localhost";
-$username = "qcxmfjwfwv";
-$password = "CxDu9p4bUT";
-$db = "qcxmfjwfwv";
+// $servername = "localhost";
+// $username = "qcxmfjwfwv";
+// $password = "CxDu9p4bUT";
+// $db = "qcxmfjwfwv";
 
 //接続用
-$con = mysqli_connect($servername, $username, $password,$db);
+// $con = mysqli_connect($servername, $username, $password,$db);
 
 if(mysqli_connect_errno())
 {
