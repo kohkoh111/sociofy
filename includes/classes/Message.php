9f69cf3b0200d33fa023bf29f11f0cd8090c8d8f
Kohki Takata <koki@Kokis-iMac.local>
2021-07-13 13:09:01 +0900

    error revised

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -136,107 +136,117 @@ class Message {
 	public function getConversations(){
 		$userLoggedIn = $this->user_obj->getUsername();
 		$return_string = "";
-		$convos = array();
+		$conversations = array();
 
-		$query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn' ORDER BY id DESC");
+		$query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn'
+																			ORDER BY id DESC");
 
-		while($row = mysqli_fetch_array($query)) {
+		while($row = mysqli_fetch_array($query)){
 			$user_to_push = ($row['user_to'] != $userLoggedIn) ? $row['user_to'] : $row['user_from'];
-
-			if(!in_array($user_to_push, $convos)) {
-				array_push($convos, $user_to_push);
+			if(!in_array($user_to_push, $conversations)){
+				array_push($conversations, $user_to_push);
 			}
 		}
-
-		foreach($convos as $username) {
+		foreach($conversations as $username){
 			$user_found_obj = new User($this->con, $username);
 			$latest_message_details = $this->getLatestMessages($userLoggedIn, $username);
 
-			$dots = (strlen($latest_message_details[1]) >= 12) ? "..." : "";
+			$dots = (strlen($latest_message_details[1] >= 12)) ? "..." : "";
 			$split = str_split($latest_message_details[1], 12);
-			$split = $split[0] . $dots; 
+			$split = $split[0] . $dots;
 
-			$return_string .= "<a href='messages.php?u=$username'> <div class='user_found_messages'>
-								<img src='" . $user_found_obj->getProfilePic() . "' style='border-radius: 5px; margin-right: 5px;'>
-								" . $user_found_obj->getFirstAndLastName() . "
-								<span class='timestamp_smaller' id='grey'> " . $latest_message_details[2] . "</span>
-								<p id='grey' style='margin: 0;'>" . $latest_message_details[0] . $split . " </p>
-								</div>
-								</a>";
+			$return_string .= "<a href='messages.php?u=$username'><div class='user_found_messages'>
+												<img src='".$user_found_obj->getProfilePic() ."' style='border-radius:5px; margin-right:5px;'>
+												". $user_found_obj->getFirstAndLastName() ."<br>
+												<span class='timestamp_smaller' id='grey'> ". $latest_message_details[2] ."</span>
+												<p id='grey' style='
+
+
+
+												margin:0;'> ". $latest_message_details[0]. $split. " </p>
+												</div>
+												</a>";
+			}
+		return $return_string;
 		}
 
-		return $return_string;
 
-	}
-
-	public function getConversationsDropdown($data, $limit) {
+	public function getConversationsDropdown($data, $limit){
 
 		$page = $data['page'];
 		$userLoggedIn = $this->user_obj->getUsername();
 		$return_string = "";
-		$convos = array();
+		$conversations = array();
 
 		if($page == 1)
 			$start = 0;
-		else 
+		else
 			$start = ($page - 1) * $limit;
 
 		$set_viewed_query = mysqli_query($this->con, "UPDATE messages SET viewed='yes' WHERE user_to='$userLoggedIn'");
 
-		$query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn' ORDER BY id DESC");
 
-		while($row = mysqli_fetch_array($query)) {
+		$query = mysqli_query($this->con, "SELECT user_to, user_from FROM messages WHERE user_to='$userLoggedIn' OR user_from='$userLoggedIn'
+																			ORDER BY id DESC");
+
+		while($row = mysqli_fetch_array($query)){
 			$user_to_push = ($row['user_to'] != $userLoggedIn) ? $row['user_to'] : $row['user_from'];
-
-			if(!in_array($user_to_push, $convos)) {
-				array_push($convos, $user_to_push);
+			if(!in_array($user_to_push, $conversations)){
+				array_push($conversations, $user_to_push);
 			}
 		}
 
 		$num_iteration = 0; //チェックしたメッセージの数
 		$count = 1; //投稿したメッセージの数
 
-		foreach($convos as $username) {
 
-			if($num_iterations++ < $start)
-				continue;
+		foreach($conversations as $username){
 
-			if($count > $limit)
-				break;
-			else 
-				$count++;
+				if($num_iteration ++ < $start)
+					continue;
 
+				if($count > $limit)
+					break;
+				else
+					$count++;
 
-			$is_unread_query = mysqli_query($this->con, "SELECT opened FROM messages WHERE user_to='$userLoggedIn' AND user_from='$username' ORDER BY id DESC");
-			$row = mysqli_fetch_array($is_unread_query);
-			$style = ($row['opened'] == 'no') ? "background-color: #DDEDFF;" : "";
+			$is_unread_array = mysqli_query($this->con, "SELECT opened FROM messages WHERE user_to='$userLoggedIn' AND user_from='$username' ORDER BY id DESC");
+			$row = mysqli_fetch_array($is_unread_array);
+			$style = (isset($row['opened']) && $row['opened'] == 'no') ? "background-color:#DDEDFF" : "";
+
 
 
 			$user_found_obj = new User($this->con, $username);
-			$latest_message_details = $this->getLatestMessage($userLoggedIn, $username);
+			$latest_message_details = $this->getLatestMessages($userLoggedIn, $username);
 
 			$dots = (strlen($latest_message_details[1]) >= 12) ? "..." : "";
 			$split = str_split($latest_message_details[1], 12);
-			$split = $split[0] . $dots; 
+			$split = $split[0] . $dots;
 
-			$return_string .= "<a href='messages.php?u=$username'> 
-								<div class='user_found_messages' style='" . $style . "'>
-								<img src='" . $user_found_obj->getProfilePic() . "' style='border-radius: 5px; margin-right: 5px;'>
-								" . $user_found_obj->getFirstAndLastName() . "
-								<span class='timestamp_smaller' id='grey'> " . $latest_message_details[2] . "</span>
-								<p id='grey' style='margin: 0;'>" . $latest_message_details[0] . $split . " </p>
-								</div>
-								</a>";
+			$return_string .= "<a href='messages.php?u=$username'>
+												<div class='user_found_messages' style='".$style."'>
+												<img src='".$user_found_obj->getProfilePic() ."' style='border-radius:5px; margin-right:5px;'>
+												". $user_found_obj->getFirstAndLastName() ."<br>
+												<span class='timestamp_smaller' id='grey'> ". $latest_message_details[2] ."</span>
+												<p id='grey' style='margin:0;'> ". $latest_message_details[0]. $split. " </p>
+												</div>
+												</a>";
+			}
+
+			if($count > $limit)
+				$return_string .= "<input type='hidden' class='nextPageDropDownData' value='".($page + 1)."'><input type='hidden' class='noMoreDropDownData' value='false'>";
+			else
+				$return_string .= "<input type='hidden' class='noMoreDropDownData' value='true'><p style='text-align:center;'>No More Posts</p>";
+
+
+
+
+
+
+
+			return $return_string;
 		}
 
-
-		if($count > $limit)
-			$return_string .= "<input type='hidden' class='nextPageDropdownData' value='" . ($page + 1) . "'><input type='hidden' class='noMoreDropdownData' value='false'>";
-		else 
-			$return_string .= "<input type='hidden' class='noMoreDropdownData' value='true'> <p style='text-align: center;'>No more messages to load!</p>";
-
-		return $return_string;
-	}
 
 	public function getUnreadNumber(){
 		$userLoggedIn = $this->user_obj->getUsername();
